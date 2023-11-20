de199698148b6e5a83f71f079899b13c5b2ce27a
Samuel Kociscak <s.kociscak@gmail.com>
2023-11-20 16:41:22 +0100

    added cnn flags location

--- paths.py
+++ paths.py
@@ -3,9 +3,11 @@ import os
 
 cdf_tswf_e_location = os.path.join("data","rpw_tds_surv_tswf_e","")
 preprocessed_location = os.path.join("data","preprocessed","")
+cnn_flags_location = os.path.join("data","cnn_flags","")	
 
 
 #%%
 if __name__ == "__main__":
     print(cdf_tswf_e_location)
     print(preprocessed_location)
+    print(cnn_flags_location)
