fe9bb2160061bfa6aa6400d76dc50acca61b1d66
Samuel Kociscak <s.kociscak@gmail.com>
2023-11-20 16:03:50 +0100

    intial

---
+++ classify.py
@@ -0,0 +1,31 @@
+import numpy as np
+
+
+
+
+def is_dust_dummy(ch1,ch2,ch3):
+    """
+    
+
+    Parameters
+    ----------
+    ch1 : np.array of float
+        Processed as described in readme.
+    ch2 : TYPE
+        DESCRIPTION.
+    ch3 : TYPE
+        DESCRIPTION.
+
+    Returns
+    -------
+    prob : TYPE
+        DESCRIPTION.
+
+    """
+
+    prob = np.random.rand()
+
+    return prob
+
+
+
--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 import numpy as np
+import sys
+from classify import is_dust_dummy
 
+
+def main(input_cdf,
+         output_flags_file):
+    """
+    The main wrapper funcrtion to load a cdf, preporcess, analyze and 
+    save the resulting CNN flag and other mtedadata in a new file.
+
+    Parameters
+    ----------
+    input_cdf : str
+        The L2 TSWF-E file to analyze.
+    output_flags_file : str
+        The output file with the CNN flag..
+
+    Returns
+    -------
+    None.
+
+    """
+    print(input_cdf)
+    print(output_flags_file)
+
+    is_dust = is_dust_dummy(ch1,ch2,ch3)
+
+    #save
+
+
+#%%
+if __name__ == "__main__":
+    main(sys.argv[1],sys.argv[2])    
