2977591eb6cb81dda2641733e4c958594e30c231
Samuel Kociscak <s.kociscak@gmail.com>
2023-11-20 16:34:44 +0100

    still kind of initial

---
+++ paths.py
@@ -0,0 +1,11 @@
+import os
+
+
+cdf_tswf_e_location = os.path.join("data","rpw_tds_surv_tswf_e","")
+preprocessed_location = os.path.join("data","preprocessed","")
+
+
+#%%
+if __name__ == "__main__":
+    print(cdf_tswf_e_location)
+    print(preprocessed_location)
