edebd78705e88ab6e192bcc6763359da2a1c530e
Samuel Kociscak <s.kociscak@gmail.com>
2023-11-22 17:54:03 +0100

    added __main__ section to be able to import functions

--- classify.py
+++ classify.py
@@ -3,21 +3,38 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.signal import medfilt
 from scipy.interpolate import interp1d
-# %% 
 
-# Read the content of the file
-file_path = '/Users/akv020/Projects/micromet-net/data/preprocessed/20211025_0463.csv'
 
-# Reading the file into a DataFrame
-data = pd.read_csv(file_path, header=None, names=['ch1', 'ch2', 'ch3'])
+# %%
+def is_dust_dummy(ch1,ch2,ch3):
+    """
+    This is a dumy function that imitates the actual "is_dust" function. 
+    The working of is_dust function is that it gets three arrays representing
+    the three TSWF-E channels and gives a verdict in the form of float between
+    0 and 1, 1 meaning it is a certain dust impact.
 
-# Dropping empty rows (if any)
-data.dropna(inplace=True)
+    Parameters
+    ----------
+    ch1 : np.array of float
+        Monopole 1, compressed, normalized, resampled.
+    ch2 : np.array of float
+        Monopole 2, compressed, normalized, resampled.
+    ch3 : np.array of float
+        Monopole 3, compressed, normalized, resampled.
 
-# Extracting each column into separate variables
-ch1 = data['ch1'].tolist()
-ch2 = data['ch2'].tolist()
-ch3 = data['ch3'].tolist()
+    Returns
+    -------
+    prob : flaot
+        The certainty that the given waveforms represent a dust impact.
+        the convention is that 0 means certain no-dust and 1 means 
+        a certain dust.
+
+    """
+
+    prob = np.random.random()
+
+    return prob
+
 
 # %%
 def so_preproc(data_raw, data_length, compression, sampling_rate):
@@ -62,67 +79,72 @@ def so_preproc(data_raw, data_length, compression, sampling_rate):
 
     return data_processed, mx, times_processed
 
-# %%
-data_length = 16384
-compression = 4
-sampling_rate = 2.6213750e+05
-d1, mx1, times_processed = so_preproc(ch1,data_length,compression,sampling_rate)
-d2, mx2, times_processed = so_preproc(ch2,data_length,compression,sampling_rate)
-d3, mx3, times_processed = so_preproc(ch3,data_length,compression,sampling_rate)
-maxi = max(mx1,mx2,mx3)
 
 #%%
-TD1 = (d1*mx1)/maxi
-TD2 = (d2*mx2)/maxi
-TD3 = (d3*mx3)/maxi
-signals = np.column_stack((TD1, TD2, TD3))
+if __name__ == "__main__":
 
-# %%
-# Creating a line plot for ch1, ch2, and ch3
-plt.figure(figsize=(12, 6))
-plt.plot(times_processed*1000,signals[:,0], label='ch1')
-plt.plot(times_processed*1000,signals[:,1], label='ch2')
-plt.plot(times_processed*1000,signals[:,2], label='ch3')
-# Adding labels and title
-plt.xlabel('time [ms]')
-plt.ylabel('Value')
-plt.title('Line Plot of ch1, ch2, and ch3')
-plt.legend()
-
-# Display the plot
-plt.show()
+    # This block of code is ran if the script is called as a main process,
+    # but not if a function is imported from the script.
 
 
+    # %%
+    
+    # Read the content of the file
+    file_path = '/Users/akv020/Projects/micromet-net/data/preprocessed/20211025_0463.csv'
 
-# %%
-def is_dust_dummy(ch1,ch2,ch3):
-    """
-    This is a dumy function that imitates the actual "is_dust" function. 
-    The working of is_dust function is that it gets three arrays representing
-    the three TSWF-E channels and gives a verdict in the form of float between
-    0 and 1, 1 meaning it is a certain dust impact.
-
-    Parameters
-    ----------
-    ch1 : np.array of float
-        Monopole 1, compressed, normalized, resampled.
-    ch2 : np.array of float
-        Monopole 2, compressed, normalized, resampled.
-    ch3 : np.array of float
-        Monopole 3, compressed, normalized, resampled.
-
-    Returns
-    -------
-    prob : flaot
-        The certainty that the given waveforms represent a dust impact.
-        the convention is that 0 means certain no-dust and 1 means 
-        a certain dust.
-
-    """
-
-    prob = np.random.rand()
-
-    return prob
+    #
+    # i accidantelly tried running the script but I couldnt because
+    # of the absolute path. I suggest the following line should work
+    # as well and independent of the computer
+    #
+    # file_path = 'data/preprocessed/20211025_0463.csv'
 
 
-
+    # Reading the file into a DataFrame
+    data = pd.read_csv(file_path, header=None, names=['ch1', 'ch2', 'ch3'])
+    
+    # Dropping empty rows (if any)
+    data.dropna(inplace=True)
+    
+    # Extracting each column into separate variables
+    ch1 = data['ch1'].tolist()
+    ch2 = data['ch2'].tolist()
+    ch3 = data['ch3'].tolist()
+    
+    
+    # %%
+    data_length = 16384
+    compression = 4
+    sampling_rate = 2.6213750e+05
+    d1, mx1, times_processed = so_preproc(ch1,data_length,compression,sampling_rate)
+    d2, mx2, times_processed = so_preproc(ch2,data_length,compression,sampling_rate)
+    d3, mx3, times_processed = so_preproc(ch3,data_length,compression,sampling_rate)
+    maxi = max(mx1,mx2,mx3)
+    
+    #%%
+    TD1 = (d1*mx1)/maxi
+    TD2 = (d2*mx2)/maxi
+    TD3 = (d3*mx3)/maxi
+    signals = np.column_stack((TD1, TD2, TD3))
+    
+    # %%
+    # Creating a line plot for ch1, ch2, and ch3
+    plt.figure(figsize=(12, 6))
+    plt.plot(times_processed*1000,signals[:,0], label='ch1')
+    plt.plot(times_processed*1000,signals[:,1], label='ch2')
+    plt.plot(times_processed*1000,signals[:,2], label='ch3')
+    # Adding labels and title
+    plt.xlabel('time [ms]')
+    plt.ylabel('Value')
+    plt.title('Line Plot of ch1, ch2, and ch3')
+    plt.legend()
+    
+    # Display the plot
+    plt.show()
+    
+    
+    
+    
+    
+    
+    
